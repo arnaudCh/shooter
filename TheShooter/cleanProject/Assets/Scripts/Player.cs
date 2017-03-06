c48808bf880b28cfed01b9b85e288a99c80082ce
Chavatanak <chavatanak.ving@gmail.com>
2017-03-07 00:58:15 +0100

    mouvement pété

---
+++ TheShooter/cleanProject/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Vector3 playerPosition;
+    public float speed = 0.15f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+
+        playerPosition.x += Input.GetAxis("Horizontal") * speed;
+        playerPosition.z += Input.GetAxis("Vertical") * speed;
+
+        transform.position = playerPosition;
+
+        Debug.Log("Horizontal " + Input.mousePosition);
+    }
+}
